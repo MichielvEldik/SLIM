c2b526138f069f9f6efcb631516112ee1473b31b
Mitchy123 <michiel_eldik@msn.com>
2020-09-19 12:13:32 +0200

    hhhh

--- question2.R
+++ question2.R
@@ -1,36 +1,51 @@
 library(ggplot2)
 library(dplyr)
 
+# Load data
 survey_data <- read.csv("/home/mitchy123/Documents/Statistical learning/SLIMSustainableFashionData.csv", sep= ",", header = TRUE)
 sc_data <- survey_data
-ggplot(sc_data, aes(x=factor(Education), y =WTR, group=Education)) + geom_boxplot()
 
+# boxplot to observe differences between education levels
+boxplot(WTR ~ Education, data = sc_data,
+        xlab = "Education", ylab = "WTR",
+        frame = FALSE, col = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+# table of mean, count, standard deviation per educational level for WTR
 new_data <- sc_data %>%
   group_by(Education) %>%
   summarise(
+    count = n(),
     mean = mean(WTR),
     sd = sd(WTR))
 new_data
 
-# Homogeneity of variances
+# Homogeneity of variances using Bartlett test
 res <- bartlett.test(WTR ~ Education, data = sc_data)
 res
 
-# Homogeneity of variances
-lev <- leveneTest(WTR ~ Education, data = sc_data)
-lev
+# Homogeneity of variances using Fligner test (more robust to non-normality)
+flig <- fligner.test(WTR ~ Education, data = sc_data)
+flig
 
-# normality tests.
+# Function for plotting qq norms
+WTR_edu_qq <- function(sc_data, edu_level) {
+  newie <- sc_data %>%
+    filter(Education == edu_level)
+  qqnorm(newie$WTR)
+}
 
+# Using the qq norm function by iterating over all education levels
+for (value in c(1, 2, 3, 4)) {
+WTR_edu_qq(sc_data, value)  
+}
 
-boxplot(WTR ~ Education, data = sc_data,
-        xlab = "Education", ylab = "WTR",
-        frame = FALSE, col = c("#00AFBB", "#E7B800", "#FC4E07"))
+# ANOVA one-way 
 WTR_AOV <- aov(WTR ~ Education, data = sc_data)
 summary(WTR_AOV)
 
-options(contrasts = c("contr.treatment", "contr.poly"))
+# ANCOVA applied using linear model 
 model_1 = lm (WTR ~ Education + PurchaseFreq,
             data = sc_data)
 summary(model_1)
+
 
